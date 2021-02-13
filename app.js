8b7bcfaa05e0929595620e40e6644677b64f04e7
Rakib544 <md.rakib10122003@gmail.com>
2021-02-13 23:19:46 +0600

    near to finish

--- app.js
+++ app.js
@@ -15,114 +15,120 @@ const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 // show images 
 const showImages = (images) => {
-  imagesArea.style.display = 'block';
-  gallery.innerHTML = '';
-  // show gallery title
-  galleryHeader.style.display = 'flex';
-  images.forEach(image => {
-    let div = document.createElement('div');
-    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
-  })
+    imagesArea.style.display = 'block';
+    gallery.innerHTML = '';
+    // show gallery title
+    galleryHeader.style.display = 'flex';
+    images.forEach(image => {
+        let div = document.createElement('div');
+        div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+        div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+        gallery.appendChild(div)
+    })
 
 }
 
 const getImages = (query) => {
-  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
-    .then(response => response.json())
-    // .then(data => console.log(data))
-    .then(data => showImages(data.hits))
-    .catch(err => console.log(err))
+    fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+        .then(response => response.json())
+        .then(data => showImages(data.hits))
+        .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
-  let element = event.target;
-  element.classList.toggle('added');
- 
-  let item = sliders.indexOf(img);
-  if (item === -1) {
-    sliders.push(img);
-  } else {
-    sliders.pop(img)
-  }
+    let element = event.target;
+    element.classList.toggle('added');
+
+    let item = sliders.indexOf(img);
+    if (item === -1) {
+        sliders.push(img);
+    } else {
+        sliders.pop(img)
+    }
 }
 var timer
 const createSlider = () => {
-  // check slider image length
-  if (sliders.length < 2) {
-    alert('Select at least 2 image.')
-    return;
-  }
-  // crate slider previous next area
-  sliderContainer.innerHTML = '';
-  const prevNext = document.createElement('div');
-  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
-  prevNext.innerHTML = ` 
+    // check slider image length
+    if (sliders.length < 2) {
+        alert('Select at least 2 image.')
+        return;
+    }
+    // crate slider previous next area
+    sliderContainer.innerHTML = '';
+    const prevNext = document.createElement('div');
+    prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+    prevNext.innerHTML = ` 
   <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
   <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
   `;
 
-  sliderContainer.appendChild(prevNext)
-  document.querySelector('.main').style.display = 'block';
-  // hide image aria
-  imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
-  if(duration < 0) {
-      alert("Time can't be negative")
-  } else {
-    sliders.forEach(slide => {
-        let item = document.createElement('div')
-        item.className = "slider-item";
-        item.innerHTML = `<img class="w-100"
+    sliderContainer.appendChild(prevNext)
+    document.querySelector('.main').style.display = 'block';
+    // hide image aria
+    imagesArea.style.display = 'none';
+    const duration = document.getElementById('duration').value || 1000;
+    if (duration < 0) {
+        alert("Time can't be negative")
+    } else {
+        sliders.forEach(slide => {
+            let item = document.createElement('div')
+            item.className = "slider-item";
+            item.innerHTML = `<img class="w-100"
         src="${slide}"
         alt="">`;
-        sliderContainer.appendChild(item)
-      })
-      changeSlide(0)
-      timer = setInterval(function () {
-        slideIndex++;
-        changeSlide(slideIndex);
-      }, duration);
-  }
-  
+            sliderContainer.appendChild(item)
+        })
+        changeSlide(0)
+        timer = setInterval(function () {
+            slideIndex++;
+            changeSlide(slideIndex);
+        }, duration);
+    }
+
 }
 
 // change slider index 
 const changeItem = index => {
-  changeSlide(slideIndex += index);
+    changeSlide(slideIndex += index);
 }
 
 // change slide item
 const changeSlide = (index) => {
 
-  const items = document.querySelectorAll('.slider-item');
-  if (index < 0) {
-    slideIndex = items.length - 1
-    index = slideIndex;
-  };
+    const items = document.querySelectorAll('.slider-item');
+    if (index < 0) {
+        slideIndex = items.length - 1
+        index = slideIndex;
+    };
 
-  if (index >= items.length) {
-    index = 0;
-    slideIndex = 0;
-  }
+    if (index >= items.length) {
+        index = 0;
+        slideIndex = 0;
+    }
 
-  items.forEach(item => {
-    item.style.display = "none"
-  })
+    items.forEach(item => {
+        item.style.display = "none"
+    })
 
-  items[index].style.display = "block"
+    items[index].style.display = "block"
 }
 
+//working on enter key button
+document.getElementById('search').addEventListener("keypress", (event) => {
+    if(event.key === 'Enter') {
+        searchBtn.click();
+    }
+})
+
 searchBtn.addEventListener('click', function () {
-  document.querySelector('.main').style.display = 'none';
-  clearInterval(timer);
-  const search = document.getElementById('search');
-  getImages(search.value)
-  sliders.length = 0;
+    document.querySelector('.main').style.display = 'none';
+    clearInterval(timer);
+    const search = document.getElementById('search');
+    getImages(search.value)
+    sliders.length = 0;
 })
 
 sliderBtn.addEventListener('click', function () {
-  createSlider()
+    createSlider()
 })
